be349e932618560b4c3f2efa5a2ad961da742f24
YOOSEUNGHYUN <ysh7733@naver.com>
2021-09-17 09:58:13 +0900

    첫번째저장

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyGold = 0;
+	float HP = 0.112122F;
+	char c = 72;
+
+	cout << MyGold << endl;
+	cout << HP << endl;
+	cout << c << endl;
+
+	return 0;
+}
